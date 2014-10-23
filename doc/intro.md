b81d29bcc1f82f3375ba76c15648f469569c39f8
Neale Swinnerton <neale@isismanor.com>
2014-10-23 09:29:42 +0100

    Initial project commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kixi.eventlog
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kixi/eventlog_test.clj
@@ -0,0 +1,7 @@
+(ns kixi.eventlog-test
+  (:require [clojure.test :refer :all]
+            [kixi.eventlog :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
