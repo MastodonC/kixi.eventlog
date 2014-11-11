1b829f92232b03395ffc9449674975d7457285ac
Neale Swinnerton <neale@isismanor.com>
2014-11-11 16:00:25 +0000

    Add some further deploy utility.
     - make instances grab a public ip
     - fn to extract ips from reservation
     - return map of everything we've created (although this is out of date
       by the time we see it because the entities have been updated) TODO -
       return updated version.

--- src/kixi/cloud/deploy.clj
+++ src/kixi/cloud/deploy.clj
@@ -12,34 +12,36 @@
         route-tables        (ec2/describe-route-tables :filters [{:name "vpc-id" :values [vpc-id]}])
         route-table-id      (get-in route-tables [:route-tables 0 :route-table-id])
         internet-gateway    (ec2/create-internet-gateway {:vpc-id vpc-id
-                                                          :cidr-block vpc-cidr-block})
+                                                          :cidr-block vpc-cidr-block
+                                                          :tags       tag})
         internet-gateway-id (get-in internet-gateway [:internet-gateway :internet-gateway-id])
         subnet              (ec2/create-subnet {:cidr-block subnet-cidr-block
                                                 :vpc-id vpc-id
-                                                :map-public-ip-on-launch true})
-        subnet-id           (get-in subnet [:subnet :subnet-id])
-        reservation         (ec2/run-instances {:image-id       "ami-6e7bd919"
-                                                :instance-type  instance-type
-                                                :vpc-id         vpc-id
-                                                :subnet-id      subnet-id
-                                                :min-count      min-count
-                                                :max-count      max-count
-                                                :key-name       key-name
-                                                :security-group security-group})
-        instances         (get-in reservation [:reservation :instances])]
+                                                :tags       tag
+                                                })
+        subnet-id           (get-in subnet [:subnet :subnet-id])]
 
     (ec2/attach-internet-gateway {:internet-gateway-id internet-gateway-id :vpc-id vpc-id})
-
     (ec2/create-route {:route-table-id route-table-id :destination-cidr-block "0.0.0.0/0" :gateway-id internet-gateway-id})
-
-    (let [elb (elb/create-load-balancer {:load-balancer-name load-balancer-name
-                                         :subnets [subnet-id]
-                                         :listeners [{:instance-port 80
-                                                      :load-balancer-port 80
-                                                      :instance-protocol "HTTP"
-                                                      :protocol "HTTP"}]})]
-      (doseq [{:keys [instance-id]} instances]
-        (elb/register-instances-with-load-balancer {:load-balancer-name load-balancer-name :instances [{:instance-id instance-id}]})))))
+    (ec2/modify-subnet-attribute {:subnet-id subnet-id
+                                  :map-public-ip-on-launch true})
+    (let [reservation (ec2/run-instances {:image-id       "ami-6e7bd919"
+                                          :instance-type  instance-type
+                                          :vpc-id         vpc-id
+                                          :subnet-id      subnet-id
+                                          :min-count      min-count
+                                          :max-count      max-count
+                                          :key-name       key-name
+                                          :security-group security-group})
+          elb         (elb/create-load-balancer {:load-balancer-name load-balancer-name
+                                                 :subnets [subnet-id]
+                                                 :listeners [{:instance-port 80
+                                                              :load-balancer-port 80
+                                                              :instance-protocol "HTTP"
+                                                              :protocol "HTTP"}]})]
+      (doseq [{:keys [instance-id]} (get-in reservation [:reservation :instances])]
+        (elb/register-instances-with-load-balancer {:load-balancer-name load-balancer-name :instances [{:instance-id instance-id}]}))
+      (merge vpcs internet-gateway subnet reservation))))
 
 (defn build []
   (build* {:vpc-cidr-block     "10.23.0.0/16"
@@ -51,3 +53,9 @@
            :max-count          5
            :tag                "mc-test-2"
            :load-balancer-name "mc-test-elb2"}))
+
+(defn extract-ip-addresses [arch]
+  (->> [:reservation :instances]
+       (get-in arch)
+       (map (juxt :instance-id (comp :private-ip-address first #(filter :primary %) :private-ip-addresses first :network-interfaces)))
+       (into {})))
