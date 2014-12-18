a5af4422d50f53d1c500be6ad1384f5fd5ac38d5
Neale Swinnerton <neale@isismanor.com>
2014-12-18 01:04:54 +0000

    Enhance zookeeper config.
    This needs to be more dynamic, like the zookeeper setup.

--- docker/start-eventlog.sh
+++ docker/start-eventlog.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-ZK01=$(grep '\bzk01\b' /etc/hosts)
-ZK02=$(grep '\bzk02\b' /etc/hosts)
-ZK03=$(grep '\bzk03\b' /etc/hosts)
+function add_hosts_entry() {
+    local var_upper=${1^^}
 
-if [ -z "${ZK01}" ]; then
-    if [ -z "${ZK01_PORT_2181_TCP_ADDR}" ]; then
-	echo "No zk01 configured, exiting..."
-	exit 1;
+    if $(grep '\b$1\b' /etc/hosts) ; then
+	echo "Retaining existing $1 entry:"
+	echo -e "\t ${!var_upper}"
+    else
+	local host_var="${var_upper}_PORT_2181_TCP_ADDR"
+	local hosts_entry="${!host_var} $1"
+	echo "Adding $1 entry"
+	echo -e "${hosts_entry}"
+	echo ${hosts_entry} >> /etc/hosts
     fi
-    hosts_entry="${ZK01_PORT_2181_TCP_ADDR} zk01"
-    echo "Adding zk01 entry:"
-    echo -e "\t${hosts_entry}"
-    echo "${hosts_entry}" >> /etc/hosts
-else
-    echo "Retaining existing zk01 entry:"
-    echo -e "\t ${ZK01}"
+}
+
+if [ -z "${ZK01_PORT_2181_TCP_ADDR}" ]; then
+    echo "No zk01 configured, exiting..."
+    exit 1;
 fi
 
-if [ -z "${ZK02}" ]; then
-    echo "${ZK02_PORT_2181_TCP_ADDR} zk02" >> /etc/hosts
-else
-    echo "Retaining existing zk02 entry:"
-    echo -e "\t ${ZK02}"
-fi
-
-if [ -z "${ZK03}" ]; then
-    echo "${ZK03_PORT_2181_TCP_ADDR} zk03" >> /etc/hosts
-else
-    echo "Retaining existing zk03 entry:"
-    echo -e "\t ${ZK03}"
-fi
+add_hosts_entry zk01
+add_hosts_entry zk02
+add_hosts_entry zk03
 
 echo "Starting uberjar..."
 java -jar /uberjar.jar
