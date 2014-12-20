c0620fe964803851da93088973f5bb5ba740ba63
Neale Swinnerton <neale@isismanor.com>
2014-12-20 22:15:42 +0000

    Fix location of Zookeeper chroot dir.

--- docker/start-eventlog.sh
+++ docker/start-eventlog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ZK_CHROOT=${ZK_CHROOT:-/}
+echo "ZK_CHROOT is ${ZK_CHROOT}"
+
+
 function join { local IFS="$1"; shift; echo "$*"; }
 
 #Add entries for zookeeper peers.
@@ -10,10 +14,10 @@ do
     zk_addr_name="${zk_name}_PORT_2181_TCP_ADDR"
     zk_port_name="${zk_name}_PORT_2181_TCP_PORT"
 
-    [ ! -z "${!zk_addr_name}" ] && hosts+=("${!zk_addr_name}:${!zk_port_name}/kafka")
+    [ ! -z "${!zk_addr_name}" ] && hosts+=("${!zk_addr_name}:${!zk_port_name}")
 done
 
-export ZK_CONNECT=$(join , ${hosts[@]})
+export ZK_CONNECT="$(join , ${hosts[@]})${ZK_CHROOT}"
 echo "Zookeeper connect string is ${ZK_CONNECT}"
 
 echo "Starting uberjar..."
