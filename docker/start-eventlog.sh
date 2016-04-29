83c3c67294f0d38800fba8905061fa379deaf129
Neale Swinnerton <neale@mastodonc.com>
2016-04-29 12:02:03 +0100

    Allow hard coded config env vars
    If values are passed in the env we don't try to discover them.

--- docker/start-eventlog.sh
+++ docker/start-eventlog.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-ZK_CHROOT=${ZK_CHROOT:-/}
-echo "ZK_CHROOT is ${ZK_CHROOT}"
-
-
 function join { local IFS="$1"; shift; echo "$*"; }
 
-#Add entries for zookeeper peers.
-hosts=()
-for i in $(seq 255)
-do
-    zk_name=$(printf "ZK%02d" ${i})
-    zk_addr_name="${zk_name}_PORT_2181_TCP_ADDR"
-    zk_port_name="${zk_name}_PORT_2181_TCP_PORT"
+if [ -z "${ZK_CONNECT}" ] ; then
 
-    [ ! -z "${!zk_addr_name}" ] && hosts+=("${!zk_addr_name}:${!zk_port_name}")
-done
+    ZK_CHROOT=${ZK_CHROOT:-/}
+    echo "ZK_CHROOT is ${ZK_CHROOT}"
 
-topics=()
-for t in {1..255}
-do
-    topic_name=$(printf "TOPIC%02d" ${t})
-    [ ! -z "${!topic_name}" ] && topics+=("\"${!topic_name}\"")
-done
+    #Add entries for zookeeper peers.
+    hosts=()
+    for i in $(seq 255)
+    do
+        zk_name=$(printf "ZK%02d" ${i})
+        zk_addr_name="${zk_name}_PORT_2181_TCP_ADDR"
+        zk_port_name="${zk_name}_PORT_2181_TCP_PORT"
 
-export TOPICS="#{${topics[@]}}"
+        [ ! -z "${!zk_addr_name}" ] && hosts+=("${!zk_addr_name}:${!zk_port_name}")
+    done
 
-export ZK_CONNECT="$(join , ${hosts[@]})${ZK_CHROOT}"
+    export ZK_CONNECT="$(join , ${hosts[@]})${ZK_CHROOT}"
+
+fi
+
+if [ -z "${TOPICS}" ]; then
+    topics=()
+    for t in {1..255}
+    do
+        topic_name=$(printf "TOPIC%02d" ${t})
+        [ ! -z "${!topic_name}" ] && topics+=("\"${!topic_name}\"")
+    done
+
+    export TOPICS="#{${topics[@]}}"
+
+fi
+
 echo "Zookeeper connect string is ${ZK_CONNECT}"
 echo "TOPICS is ${TOPICS}"
 
